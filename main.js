68d947bdc757a1767ef704eeee27b1a3b4eeaf2b
Alex <89929071+AlexBreskolenko@users.noreply.github.com>
2024-06-29 19:07:17 +0300

    Task 4 completed

--- main.js
+++ main.js
@@ -14,12 +14,27 @@
 // test === true ? console.log("+++") : console.log("---");
 
 //*********Task 3************ */
-const day = 21;
+// const day = 21;
 
-if (day === 1 || day <= 10) {
-  console.log("Первая декада.");
-} else if (day >= 11 && day <= 20) {
-  console.log("Вторая декада.");
+// if (day === 1 || day <= 10) {
+//   console.log("Первая декада.");
+// } else if (day >= 11 && day <= 20) {
+//   console.log("Вторая декада.");
+// } else {
+//   console.log("Третья декада.");
+// }
+//*********Task 4************ */
+const number = Number(prompt("Введите число:"));
+
+if (number === 0 || number < 0) {
+  console.log("Число равно 0 или число отрицательное.");
 } else {
-  console.log("Третья декада.");
+  let hundred = (number / 100) % 10;
+  let dozent = (number / 10) % 10;
+  let unit = number % 10;
+  console.log(
+    `В числе ${number} количество\nсотен - ${Math.round(
+      hundred
+    )}\nдесятков - ${Math.round(dozent)}\nединиц - ${unit}`
+  );
 }
