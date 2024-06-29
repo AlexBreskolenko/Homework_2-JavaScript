0c9fedef6369750118aa4d61b20545aff2774fed
Alex <89929071+AlexBreskolenko@users.noreply.github.com>
2024-06-29 18:05:45 +0300

    Task 2 completed

--- main.js
+++ main.js
@@ -1,11 +1,14 @@
 //*********Task 1************ */
-const numOne = Number(prompt("Введите первое число :"));
-const numTwo = Number(prompt("Введите второе число :"));
+// const numOne = Number(prompt("Введите первое число :"));
+// const numTwo = Number(prompt("Введите второе число :"));
 
-if (numOne <= 1 && numTwo >= 3) {
-  console.log(
-    "Первое число меньше или равно 1, второе число больше или равно 3"
-  );
-} else {
-  console.log("Условие не выполнено.");
-}
+// if (numOne <= 1 && numTwo >= 3) {
+//   console.log(
+//     "Первое число меньше или равно 1, второе число больше или равно 3"
+//   );
+// } else {
+//   console.log("Условие не выполнено.");
+// }
+//*********Task 2************ */
+// let test = true;
+// test === true ? console.log("+++") : console.log("---");
