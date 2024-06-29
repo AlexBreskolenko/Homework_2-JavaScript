c063c81c50262ffcd34df61039a93c457810de11
Alex <89929071+AlexBreskolenko@users.noreply.github.com>
2024-06-29 18:03:33 +0300

    Task one completed

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
+//*********Task 1************ */
+const numOne = Number(prompt("Введите первое число :"));
+const numTwo = Number(prompt("Введите второе число :"));
 
+if (numOne <= 1 && numTwo >= 3) {
+  console.log(
+    "Первое число меньше или равно 1, второе число больше или равно 3"
+  );
+} else {
+  console.log("Условие не выполнено.");
+}
