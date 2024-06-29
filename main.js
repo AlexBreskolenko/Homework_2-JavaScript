6fba3b3a47a94ed26abd66cb8c8b764a071bfcac
Alex <89929071+AlexBreskolenko@users.noreply.github.com>
2024-06-29 19:08:09 +0300

    homework completed

--- main.js
+++ main.js
@@ -1,28 +1,28 @@
 //*********Task 1************ */
-// const numOne = Number(prompt("Введите первое число :"));
-// const numTwo = Number(prompt("Введите второе число :"));
+const numOne = Number(prompt("Введите первое число :"));
+const numTwo = Number(prompt("Введите второе число :"));
 
-// if (numOne <= 1 && numTwo >= 3) {
-//   console.log(
-//     "Первое число меньше или равно 1, второе число больше или равно 3"
-//   );
-// } else {
-//   console.log("Условие не выполнено.");
-// }
-//*********Task 2************ */
-// let test = true;
-// test === true ? console.log("+++") : console.log("---");
+if (numOne <= 1 && numTwo >= 3) {
+  console.log(
+    "Первое число меньше или равно 1, второе число больше или равно 3"
+  );
+} else {
+  console.log("Условие не выполнено.");
+}
+/*********Task 2************ */
+let test = true;
+test === true ? console.log("+++") : console.log("---");
 
 //*********Task 3************ */
-// const day = 21;
+const day = 21;
 
-// if (day === 1 || day <= 10) {
-//   console.log("Первая декада.");
-// } else if (day >= 11 && day <= 20) {
-//   console.log("Вторая декада.");
-// } else {
-//   console.log("Третья декада.");
-// }
+if (day === 1 || day <= 10) {
+  console.log("Первая декада.");
+} else if (day >= 11 && day <= 20) {
+  console.log("Вторая декада.");
+} else {
+  console.log("Третья декада.");
+}
 //*********Task 4************ */
 const number = Number(prompt("Введите число:"));
 
